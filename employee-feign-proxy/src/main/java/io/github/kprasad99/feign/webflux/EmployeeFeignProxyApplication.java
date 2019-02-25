212f017ccea1b3bb81dda5575a5c8bae1e4b8aec
Karthik Prasad <adkp.mlr@gmail.com>
2019-02-25 16:43:50 +0530

    FIX: static file resolve issue

--- employee-feign-proxy/src/main/java/io/github/kprasad99/feign/webflux/EmployeeFeignProxyApplication.java
+++ employee-feign-proxy/src/main/java/io/github/kprasad99/feign/webflux/EmployeeFeignProxyApplication.java
@@ -3,6 +3,7 @@ package io.github.kprasad99.feign.webflux;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableFeignClients
